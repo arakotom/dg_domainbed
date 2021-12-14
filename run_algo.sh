a41a387066eae1a7df69251ee5d85c0150539133
arakotom <alain.rakotomamonjy@gmail.com>
2021-12-14 23:30:41 +0100

    test

--- run_algo.sh
+++ run_algo.sh
@@ -14,3 +14,12 @@ do
 	python train.py --dataset RotatedMNIST --test_envs $t --algorithm $a
 done
 done
+
+
+for a in ClassCORAL CORAL ERM
+do
+for t in 0 1 2  3    # a stands for the algorithm
+do
+	python train.py --dataset PACS --test_envs $t --algorithm $a
+done
+done
