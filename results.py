df2b32f4a13e13705bd606be6aaaf4dd341436b0
arakotom <alain.rakotomamonjy@gmail.com>
2021-12-15 13:11:48 +0100

    test

--- results.py
+++ results.py
@@ -17,16 +17,17 @@ algo_list = ['ERM', 'CORAL','ClassCORAL']
 data = 'RotatedMNIST'
 output_dir = './results/'
 for algo in algo_list:
-    with open(os.path.join(output_dir,data, algo,'results-[1]'), 'r') as myfile:
-        lines=myfile.readlines()
-
-
+    for i_envs in env_list:
+        with open(os.path.join(output_dir,data, algo,'results-[1]'), 'r') as myfile:
+            lines=myfile.readlines()
     
-    obj = json.loads(lines[-1])
-    test_envs = obj['args']['test_envs'][0]
-    perf_in  = obj[f"env{test_envs}_in_acc"]
-    perf_out  = obj[f"env{test_envs}_out_acc"]
-
-    print(f'{algo} \t\t\t {perf_in:2.3f} \t {perf_out}')
+    
         
+        obj = json.loads(lines[-1])
+        test_envs = obj['args']['test_envs'][i_envs]
+        perf_in  = obj[f"env{test_envs}_in_acc"]
+        perf_out  = obj[f"env{test_envs}_out_acc"]
+    
+        print(f'{algo} \t\t\t {perf_in:2.3f} \t {perf_out}')
+            
 
