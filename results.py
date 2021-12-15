411ea014ffe30c5d0e862e1180348dd441fae73b
arakotom <alain.rakotomamonjy@gmail.com>
2021-12-15 12:06:36 +0100

    test

--- results.py
+++ results.py
@@ -18,11 +18,11 @@ data = 'RotatedMNIST'
 output_dir = './results/'
 for algo in algo_list:
     with open(os.path.join(output_dir,data, algo,'results-[1]'), 'r') as myfile:
-        data=myfile.readlines()
+        lines=myfile.readlines()
 
 
     
-    obj = json.loads(data[-1])
+    obj = json.loads(lines[-1])
     test_envs = obj['args']['test_envs'][0]
     perf_in  = obj[f"env{test_envs}_in_acc"]
     perf_out  = obj[f"env{test_envs}_out_acc"]
