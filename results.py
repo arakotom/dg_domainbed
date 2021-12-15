dcb71c21b3db2be765d940293dac0b7732979bc8
arakotom <alain.rakotomamonjy@gmail.com>
2021-12-15 13:55:53 +0100

    test

--- results.py
+++ results.py
@@ -17,8 +17,12 @@ algo_list = ['ERM', 'CORAL','ClassCORAL']
 env_list = [0,1,2,3]
 data = 'PACS'
 output_dir = './results/'
-for algo in algo_list:
-    for i_envs in env_list:
+score = ['']*len(env_list)
+score_mat = np.zeros(len(algo_list),len(env_list))
+for i_envs in env_list:
+    for k, algo in enumerate(algo_list):
+        score[i_envs].append(algo)
+
         with open(os.path.join(output_dir,data, algo,f'results-[{i_envs:}]'), 'r') as myfile:
             lines=myfile.readlines()
     
@@ -29,6 +33,10 @@ for algo in algo_list:
         perf_in  = obj[f"env{test_envs}_in_acc"]
         perf_out  = obj[f"env{test_envs}_out_acc"]
     
-        print(f'{algo} \t\t\t {perf_in:2.3f} \t {perf_out}')
+        score_mat[k,i_envs] = perf_out
+    
+    
+print(score_mat)
+#print(f'{algo} \t\t\t {perf_in:2.3f} \t {perf_out}')
             
 
