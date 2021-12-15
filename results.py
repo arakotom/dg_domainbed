929654671fc5a3947f2069ba48997ad814a87641
arakotom <alain.rakotomamonjy@gmail.com>
2021-12-15 14:13:00 +0100

    test

--- results.py
+++ results.py
@@ -18,7 +18,10 @@ env_list = [0,1,2,3]
 data = 'PACS'
 output_dir = './results/'
 score = ['']*len(env_list)
-score_mat = np.zeros((len(algo_list),len(env_list)))
+score_mat_test = np.zeros((len(algo_list),len(env_list)))
+score_mat_traindomain = np.zeros((len(algo_list),len(env_list)))
+
+
 for i_envs in env_list:
     for k, algo in enumerate(algo_list):
         #score[i_envs].append(algo)
@@ -30,13 +33,19 @@ for i_envs in env_list:
         
         obj = json.loads(lines[-1])
         test_envs = obj['args']['test_envs'][0]
-        perf_in  = obj[f"env{test_envs}_in_acc"]
-        perf_out  = obj[f"env{test_envs}_out_acc"]
+        perf_target_out  = obj[f"env{test_envs}_out_acc"]
     
-        score_mat[k,i_envs] = perf_out
+        perf_source_out = 0
+        for j in env_list:
+            if j !=i_envs : # j is not target domain
+                perf_source_out+= obj[f"env{test_envs}_out_acc"]
+        perf_source_out/= (len(env_list)-1)
+        score_mat_test[k,i_envs] = perf_target_out
+        score_mat_traindomain[k,i_envs] = perf_source_out
+
     
-    
-print(score_mat)
+print(score_mat_test)
+print(score_mat_traindomain)
 #print(f'{algo} \t\t\t {perf_in:2.3f} \t {perf_out}')
             
 
