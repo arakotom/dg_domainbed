ef61315934e33d0fe8d496db2f493f2b92a030fa
arakotom <alain.rakotomamonjy@gmail.com>
2021-12-15 13:10:32 +0100

    test

--- results.py
+++ results.py
@@ -27,6 +27,6 @@ for algo in algo_list:
     perf_in  = obj[f"env{test_envs}_in_acc"]
     perf_out  = obj[f"env{test_envs}_out_acc"]
 
-    print(f'{algo} \t {perf_in:2.3f} \t {perf_out}')
+    print(f'{algo} \t\t\t {perf_in:2.3f} \t {perf_out}')
         
 
