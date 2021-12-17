ce912a6a573dac66079bc61e8d2db4eccdc8653f
arakotom <alain.rakotomamonjy@gmail.com>
2021-12-18 00:23:55 +0100

    test

--- run_algo.sh
+++ run_algo.sh
@@ -21,17 +21,11 @@ for a in ClassCORAL CORAL ERM # a stands for the algorithm
 do
 for t in 0 1 2  3     # a stands for the algorithm
 do
-    if [[ $a -eq ERM ]]
-    then
-    python train.py --dataset PACS --test_envs $t --algorithm $a --steps $s
-    fi
-    if [[ $a -ne ERM ]]
-    then
+
 	python train.py --dataset PACS --test_envs $t --algorithm $a --mmd_gamma 1 --steps $s
 	python train.py --dataset PACS --test_envs $t --algorithm $a --mmd_gamma 0.5 --steps $s
 	python train.py --dataset PACS --test_envs $t --algorithm $a --mmd_gamma 2 --steps $s
     
-    fi
 done
 done
 done
