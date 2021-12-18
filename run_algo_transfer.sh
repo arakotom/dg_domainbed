e72ea21abb2c7939550c18549656b8e71d249de8
arakotom <alain.rakotomamonjy@gmail.com>
2021-12-18 19:32:34 +0100

    test

---
+++ run_algo_transfer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Clear the DISPLAY.
+# unsetenv DISPLAY
+# Call MATLAB with the appropriate input and output,
+# make it immune to hangups and quits using ''nohup'',
+# and run it in the background.
+
+
+
+#for a in ClassCORAL CORAL ERM
+#do
+#for t in 0 1 2  3 4    # a stands for the algorithm
+#do
+#	python train.py --dataset RotatedMNIST --test_envs $t --algorithm $a
+#done
+#done
+
+
+
+
+for t in 0 1 2  3     # a stands for the algorithm
+do
+    python train.py --dataset PACS --algorithm Transfer --test_envs $t 
+
+
+done
