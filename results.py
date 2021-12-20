4f9fa4e776c68fae029326a88000a1cc5c284b79
arakotom <alain.rakotomamonjy@gmail.com>
2021-12-20 20:05:39 +0100

    test

--- results.py
+++ results.py
@@ -31,24 +31,25 @@ for i_envs in env_list:
     for k, algo in enumerate(algo_list):
         #score[i_envs].append(algo)
 
-
-        with open(os.path.join(output_dir,data, algo,f'results-[{i_envs:}]'), 'r') as myfile:
-            lines=myfile.readlines()
-    
-    
+        try:
+            with open(os.path.join(output_dir,data, algo,f'results-[{i_envs:}]'), 'r') as myfile:
+                lines=myfile.readlines()
         
-        obj = json.loads(lines[-1])
-        test_envs = obj['args']['test_envs'][0]
-        perf_target_out  = obj[f"env{test_envs}_out_acc"]
-    
-        perf_source_out = 0
-        for j in env_list:
-            if j !=i_envs : # j is not target domain
-                perf_source_out+= obj[f"env{j}_out_acc"]
-        perf_source_out/= (len(env_list)-1)
-        score_mat_test[k,i_envs] = perf_target_out
-        score_mat_traindomain[k,i_envs] = perf_source_out
-
+        
+            
+            obj = json.loads(lines[-1])
+            test_envs = obj['args']['test_envs'][0]
+            perf_target_out  = obj[f"env{test_envs}_out_acc"]
+        
+            perf_source_out = 0
+            for j in env_list:
+                if j !=i_envs : # j is not target domain
+                    perf_source_out+= obj[f"env{j}_out_acc"]
+            perf_source_out/= (len(env_list)-1)
+            score_mat_test[k,i_envs] = perf_target_out
+            score_mat_traindomain[k,i_envs] = perf_source_out
+        except:
+            pass
     
 print(score_mat_test)
 print(score_mat_traindomain)
