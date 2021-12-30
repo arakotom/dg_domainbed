fcb735a95815f55f0977f933f20d38ad1cf63dcf
arakotom <alain.rakotomamonjy@gmail.com>
2021-12-30 22:41:09 +0100

    test

---
+++ kill_algo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# Clear the DISPLAY.
+# unsetenv DISPLAY
+# Call MATLAB with the appropriate input and output,
+# make it immune to hangups and quits using ''nohup'',
+# and run it in the background.
+
+
+
+import os
+import getopt,sys
+
+
+
+
+for _ in range(100):
+    os.system('killall python')
+
+
+
+
+
