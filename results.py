1d4c123324771bb1e49351d022a98268daddf468
arakotom <alain.rakotomamonjy@gmail.com>
2022-01-02 16:38:08 +0100

    test

--- results.py
+++ results.py
@@ -84,7 +84,7 @@ for i_envs in env_list:
 
 
 
-algo_to_showlist = ['CORAL','ClassMMD','ClassCORAL']
+algo_to_showlist = ['CORAL','ClassMMD','ClassCORAL','ClassWD']
 for algo_to_show in algo_to_showlist:
     for i_seed in range(4):
         for k, algo in enumerate((algo_list)):
